bed359117c766773247fb46201a98fe59eeabc57
Handziuk Volodymyr <vova13112002vova@gmail.com>
2023-02-11 15:43:12 +0200

    newLab04

---
+++ Lab04/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MainApiComponent } from './main-api/main-api.component';
+import { NewServiceService } from './new-service.service';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewComponentComponent,
+    MainApiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [NewServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Lab04/src/app/main-api/main-api.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-api',
+  templateUrl: './main-api.component.html',
+  styleUrls: ['./main-api.component.scss']
+})
+
+export class MainApiComponent {
+  userName: string = "";
+  response: any;
+  constructor(private http: HttpClient) {
+  }
+  search() {
+    this.http.get('https://api.github.com/users/' + this.userName)
+      .subscribe((response) => {
+        this.response = response;
+        console.log(this.response);
+      })
+  }
+}
