6ec9ba42c76d537fac7cc8c3e1e2aed2ccb24930
Handziuk Volodymyr <vova13112002vova@gmail.com>
2023-02-12 15:17:43 +0200

    testLB4

--- Lab04/src/app/app.module.ts
+++ Lab04/src/app/app.module.ts
@@ -7,17 +7,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { MainApiComponent } from './main-api/main-api.component';
 import { NewServiceService } from './new-service.service';
 import { FormsModule } from '@angular/forms';
+import { ContactuComponent } from './contactu/contactu.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },//путь і компонент до головної сторінки
+  { path: 'contactu', component: ContactuComponent },//путь і компонент до Контактів
+  { path: 'git', component: MainApiComponent }//путь і компонент до Резюме
+]
 @NgModule({
   declarations: [
     AppComponent,
     NewComponentComponent,
-    MainApiComponent
+    MainApiComponent,
+    ContactuComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)//які адреса будуть відслідовуватися і передаємо змінну з адресами
   ],
   providers: [NewServiceService],
   bootstrap: [AppComponent]
